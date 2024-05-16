0eb9eef34b9bd1848c4428a3a7a5a540bef06730
Yuchan Han <serltretu24@gmail.com>
2024-05-16 15:29:54 +0900

    feat: add post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
 import { useState, useEffect } from 'react';
 
 function Post({ id }) {
+    const [post, setPost] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        .then((response) => response.json())
+        .then((data) => {
+            setPost(data.body);
+            setLoading(false);
+        });
+    }, [id]);
+
+    if (loading) {
+      return <p>loading</p>;
+    }
+
+    return <p>{post}</p>;
 
 };
 
 function Counter() {
     const [count, setCount] = useState(0);
     return (
-      <>
         <button onClick={() => setCount(count + 1)}>
           count: {count}
         </button>
-      </>
     );
 };
 
 function App() {
   return (
-    <>
       <div>
         <Counter />
         <Post id={1} />
       </div>
-    </>
   );
 };
 
