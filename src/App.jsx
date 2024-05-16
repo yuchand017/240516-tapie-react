698434829b7f6fee655578c9ae2f1f63346d3fe6
Yuchan Han <serltretu24@gmail.com>
2024-05-16 15:23:16 +0900

    feat: add counter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-function App() {
+import { useState, useEffect } from 'react';
 
+function Post({ id }) {
+
+};
+
+function Counter() {
+    const [count, setCount] = useState(0);
+    return (
+      <>
+        <button onClick={() => setCount(count + 1)}>
+          count: {count}
+        </button>
+      </>
+    );
+};
+
+function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <div>
+        <Counter />
+        <Post id={1} />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
